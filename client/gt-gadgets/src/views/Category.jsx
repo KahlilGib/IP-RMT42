25a8e1a65709f8064bc7b6550a8249f9b9b01728
Kahlil Gibran <kgibran53@gmail.com>
2023-11-24 07:11:50 +0700

    last commit

--- client/gt-gadgets/src/views/Category.jsx
+++ client/gt-gadgets/src/views/Category.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TableCategory from '../components/TableCategory'
+import Navbar from '../components/sidebar'
 
 
 export default function Category() {
   return (
     <>
+    <Navbar></Navbar>
     <div
     className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
   >
